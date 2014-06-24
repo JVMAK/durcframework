88569523f5d6c68785f0bc507a71c5d6c07ff85c
durcframework <thc8775@163.com>
2014-06-24 11:36:16 +0800

    删除不必要的类

--- src/main/java/org/durcframework/common/WebContext.java
+++ src/main/java/org/durcframework/common/WebContext.java
@@ -3,21 +3,12 @@ package org.durcframework.common;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-/**
- * 设置request,response对象.方便在其它地方调用
- * 每次请求都会设置一下<br>
- * 详见:org.egula.framework.servlet.DispatcherServlet.service(HttpServletRequest, HttpServletResponse)
- * 使用方法:<br>
- * WebContext.get().getRequest();<br>
- * @author hc.tang
- * 2013-4-11
- */
 public enum WebContext {
 	INSTANCE;
 	
 	private ThreadLocal<HttpServletRequest> request = new ThreadLocal<HttpServletRequest>();
 	
-	public static WebContext get() {
+	public static WebContext getInstance() {
 		return INSTANCE;
 	}
 	
